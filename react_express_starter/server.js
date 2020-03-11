d676a14eda9248d5db75bdbc60e44cdf56e2ba3f
Albert Yee <albertyee84@gmail.com>
2020-03-11 12:53:28 -0700

    random projects

--- react_express_starter/server.js
+++ react_express_starter/server.js
@@ -2,14 +2,22 @@ const express = require('express');
 
 const app = express();
 
+const customers = [
+  {id: 1, firstName: 'John', lastName: 'Doe'},
+  {id: 2, firstName: 'Brad', lastName: 'Traversy'},
+  {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+];
+
+app.use(express.json());
+
 app.get('/api/customers', (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
 
   res.json(customers);
+});
+
+app.post('/api/customers', (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
 });
 
 const port = 5000;
